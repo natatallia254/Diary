43a15a7072c6aee28b34de4cfe4ad94dacd7723f
Natalia Brukovskaya <brukovskaanatala@gmail.com>
2023-01-18 14:37:13 +0300

    Diary

---
+++ src/test/java/DiaryTestCase.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+class DiaryTestCase {
+
+    private WebDriver driver;
+    private Diary diary;
+
+    @BeforeClass
+    public void beforeDiaryTest(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://diary.ru/user/registration");
+        diary = new Diary(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));   //неявное ожидание
+    }
+
+    @Test
+    public void DiaryTest(){
+        diary.enterUserAndEmail("natallia7539724","natallia7539724@yandex.ru");
+        diary.checkCheckBox();
+        diary.clickButton();
+        Assert.assertTrue(diary.avatarVisible());
+    }
+
+    @AfterClass
+    public void afterDiaryTest(){
+        driver.quit();
+    }
+}
